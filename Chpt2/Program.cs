aed7bf9c2b97f7be2680bb41da437f01297134d6
binlyz <binlyzhuo@outlook.com>
2017-04-18 20:24:15 +0800

    ~

---
+++ Chpt2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chpt2
+{
+    class Program
+    {
+        delegate void StringProcessor(string input);
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
