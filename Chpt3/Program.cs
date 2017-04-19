21ff16456e666c908787a772a5833b75cda9d808
binlyz <binlyzhuo@outlook.com>
2017-04-19 20:58:36 +0800

    ~

--- Chpt3/Program.cs
+++ Chpt3/Program.cs
@@ -9,6 +9,22 @@ namespace Chpt3
     {
         static void Main(string[] args)
         {
+            TypeWithField<int>.field = "string";
+            TypeWithField<int>.PrintField();
+
+
+            TypeWithField<DateTime>.field = "Datetime";
+            TypeWithField<DateTime>.PrintField();
+            Console.ReadLine();
+        }
+    }
+
+    class TypeWithField<T>
+    {
+        public static string field;
+        public static void PrintField()
+        {
+            Console.WriteLine(field+":"+typeof(T).Name);
         }
     }
 }
