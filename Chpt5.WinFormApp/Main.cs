b1d1caaa640d4bf300be7237af15e712ee115118
binlyz <binlyzhuo@outlook.com>
2017-04-22 12:42:38 +0800

    ~

--- Chpt5.WinFormApp/Main.cs
+++ Chpt5.WinFormApp/Main.cs
@@ -16,5 +16,7 @@ namespace Chpt5.WinFormApp
         {
             InitializeComponent();
         }
+
+
     }
 }
--- Chpt5/Program.cs
+++ Chpt5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Chpt5
 {
@@ -12,14 +13,32 @@ namespace Chpt5
         {
             Console.WriteLine("");
 
-
+            Button btn = new Button();
+            btn.Text = "Click me!!";
+            btn.Click += new EventHandler(LogPlainEvent);
+            btn.KeyPress += new KeyPressEventHandler(LogKeyEvent);
+            btn.MouseClick += new MouseEventHandler(LogMouseEvent);
+            Form form = new Form();
+            form.Controls.Add(btn);
+            form.AutoSize = true;
+            Application.Run(form);
             Console.ReadLine();
         }
 
 
         static void LogPlainEvent(object obj,EventArgs e)
         {
+            Console.WriteLine("LogPlain");
+        }
 
+        static void LogKeyEvent(object obj,KeyPressEventArgs e)
+        {
+            Console.WriteLine("LogKey");
+        }
+
+        static void LogMouseEvent(Object obj,MouseEventArgs e)
+        {
+            Console.WriteLine("LogMouse");
         }
     }
 }
