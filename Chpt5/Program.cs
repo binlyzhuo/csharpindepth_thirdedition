2ba5cca9a2afe7b84895f40b9d1ddf910796cc8f
binlyz <binlyzhuo@outlook.com>
2017-04-22 15:17:14 +0800

    ~

--- Chpt5/Program.cs
+++ Chpt5/Program.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Chpt5
 {
     class Program
     {
+        delegate Stream StreamFactory();
         static void Main(string[] args)
         {
             Console.WriteLine("");
 
             Button btn = new Button();
             btn.Text = "Click me!!";
-            btn.Click += new EventHandler(LogPlainEvent);
-            btn.KeyPress += new KeyPressEventHandler(LogKeyEvent);
-            btn.MouseClick += new MouseEventHandler(LogMouseEvent);
+            //btn.Click += LogPlainEvent;
+            //btn.KeyPress += LogPlainEvent;
+            btn.MouseClick += LogMouseEvent;
             Form form = new Form();
             form.Controls.Add(btn);
             form.AutoSize = true;
@@ -39,6 +42,37 @@ namespace Chpt5
         static void LogMouseEvent(Object obj,MouseEventArgs e)
         {
             Console.WriteLine("LogMouse");
+
+            ThreadStart ts = new ThreadStart(MyMethod);
+            ThreadStart ts2 = MyMethod;
+            Thread t = new Thread(ts2);
+            t.Start();
+
+            StreamFactory factory = GenerateSampleData;
+            using (Stream stream = factory())
+            {
+                int data;
+                while((data=stream.ReadByte())!=-1)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+
+        static void MyMethod()
+        {
+            Console.WriteLine("MyMethod!!");
+        }
+
+        static MemoryStream GenerateSampleData()
+        {
+            byte[] buffer = new byte[16];
+            for(int i=0;i<buffer.Length;i++)
+            {
+                buffer[i] = (byte)i;
+            }
+
+            return new MemoryStream(buffer);
         }
     }
 }
