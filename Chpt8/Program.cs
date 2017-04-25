23b849bd36d5c35bea48a692a10959c53a979e3b
binlyz <binlyzhuo@outlook.com>
2017-04-25 21:03:20 +0800

    ~

--- Chpt8/Program.cs
+++ Chpt8/Program.cs
@@ -11,7 +11,43 @@ namespace Chpt8
         static void Main(string[] args)
         {
             Console.WriteLine("Chpt8");
+
+            Person p1 = new Person("user001",10);
+            Person p2 = new Person("user002",20);
+            Person p3 = new Person("user003",30);
+            p3.Dispose();
+            Console.WriteLine(Person.InstanceCounter);
             Console.ReadLine();
+        }
+    }
+
+    public class Person:IDisposable
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public static int InstanceCounter { set; get; }
+        private static readonly object countLocker = new object();
+
+        public Person(string name,int age)
+        {
+            this.Name = name;
+            this.Age = age;
+
+            lock(countLocker)
+            {
+                InstanceCounter++;
+            }
+        }
+
+        
+        public void Dispose()
+        {
+            //base.Dispose();
+            lock (countLocker)
+            {
+                InstanceCounter--;
+            }
         }
     }
 }
