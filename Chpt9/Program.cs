ffea0dbc7194c9e871114c955e6b153d694a5893
binlyz <binlyzhuo@outlook.com>
2017-04-26 20:26:24 +0800

    ~

--- Chpt9/Program.cs
+++ Chpt9/Program.cs
@@ -10,7 +10,12 @@ namespace Chpt9
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Chpt9");
+            Func<string, int> returnLen = delegate (string txt) { return txt.Length; };
+            Func<string, int> getLen = (txt) => txt.Length;
+            Console.WriteLine(getLen("Hello"));
+            Console.WriteLine(returnLen("get"));
+            Console.ReadLine();
         }
     }
 }
